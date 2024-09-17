136144956966d69b175cff3bfe2a3f8fef12fca7
Ani Khachatryan <AKhachatryan@fxcess.com>
2024-09-17 14:32:02 +0400

    added empty projects for Clean Architecture (Application, Domain, Infrastructure,Common, Tests)

---
+++ Warehouse.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Warehouse.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Warehouse.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Warehouse.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
