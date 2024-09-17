aaac222a3126e5f35511e495bc4e245bebdbe198
Ani Khachatryan <AKhachatryan@fxcess.com>
2024-09-17 16:49:49 +0400

    added LowStock and OutOfStock thresholds into Category, removed StockConfiguration entity

--- Warehouse.Domain/Entities/StockConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Warehouse.Domain.Entities
-{
-    public class StockConfiguration
-    {
-        public int ProductId { get; set; }
-        public int LowStockThreshold { get; set; }
-        public int OutOfStockThreshold { get; set; }
-    }
-}
