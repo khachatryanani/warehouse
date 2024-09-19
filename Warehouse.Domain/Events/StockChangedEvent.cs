c7fb71b694d07524291781aaf29b3652662ea365
Ani Khachatryan <AKhachatryan@fxcess.com>
2024-09-19 10:53:51 +0400

    added stock changed event and correcposing consumer, publishing stockchanged event when product stock items count is udpated manually

---
+++ Warehouse.Domain/Events/StockChangedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Warehouse.Domain.Events
+{
+    public class StockChangedEvent
+    {
+        public int ProductId { get; set; }
+
+        public int StockItemsCount { get; set; }
+    }
+}
---
+++ Warehouse.Infrastructure/Consumers/StockChangedConsumer.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using Warehouse.Domain.Abstractions;
+using Warehouse.Domain.Enums;
+using Warehouse.Domain.Events;
+
+namespace Warehouse.Infrastructure.Consumers
+{
+    public class StockChangedConsumer(IOrderRepository orderRepository, IPublishEndpoint publishEndpoint) : IConsumer<StockChangedEvent>
+    {
+        public async Task Consume(ConsumeContext<StockChangedEvent> context)
+        {
+            var order = (await orderRepository.GetByProductIdAsync(context.Message.ProductId))
+                                              .FirstOrDefault(o => o.Status is OrderStatus.Pending && context.Message.StockItemsCount >= o.ItemsCount);
+            if (order is null)
+            {
+                return;
+            }
+
+            await publishEndpoint.Publish<OrderCreatedEventResponse>(new OrderCreatedEvent()
+            {
+                CorrelationId = Guid.NewGuid(),
+                OrderId = order.Id,
+                Mode = ReserveMode.ReserveWhenAvailable,
+            }, cancellationToken: context.CancellationToken);
+        }
+    }
+}
