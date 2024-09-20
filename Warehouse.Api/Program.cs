4fd248a74e04f865a9012882ae1cb8c18ac96e34
Ani Khachatryan <AKhachatryan@fxcess.com>
2024-09-20 11:58:05 +0400

    added docker files and docker-compose.yml

--- Warehouse.Api/Program.cs
+++ Warehouse.Api/Program.cs
@@ -22,12 +22,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddFluentValidationRulesToSwagger();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseMiddleware<ExceptionHandlerMiddleware>();
 app.UseHttpsRedirection();
 
